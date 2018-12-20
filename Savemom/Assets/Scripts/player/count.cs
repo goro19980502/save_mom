07b4270e1470839b1873a0b2455c8898f9ee3eec
Shawn Hsieh <goro19980502@gmail.com>
2018-12-20 20:54:12 +0800

    reset gameScore

--- Savemom/Assets/Scripts/player/count.cs
+++ Savemom/Assets/Scripts/player/count.cs
@@ -6,19 +6,24 @@ using UnityEngine.UI;
 public class count : MonoBehaviour {
 
 	int SUM = 0;
+    int len = 0;
 	public GameObject[] eleobj;
 	public GameObject GameWinnerCanvas;
 	public GameObject UICanvas;
     public GameObject GameManager;
-	void Update () {
-		if (eleobj == null)
-			eleobj = GameObject.FindGameObjectsWithTag("box");
+    void Start()
+    {
+        if (eleobj == null)
+            eleobj = GameObject.FindGameObjectsWithTag("box");
+        len = eleobj.Length;
+    }
+    void Update () {
         Debug.Log("eleobj.len" + eleobj.Length);
-		for (int i = 0; i < eleobj.Length; i++)
+		for (int i = 0; i < len; i++)
 		{
 			if (eleobj[i].activeInHierarchy == false)
 				SUM++;
-			if (SUM == eleobj.Length)
+			if (SUM == len)
 			{
 				OnWinnerGame();
 			}
